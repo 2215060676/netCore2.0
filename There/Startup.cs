8ed6165966fd6ffb393c6befd778bd53940684bc
2215060676@qq.com <2215060676@qq.com>
2020-04-24 15:52:56 +0800

    添加项目文件。

---
+++ There/Startup.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace There
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+        // IConfiguration configUration net core内有依赖注入，配置文件可以写在appsettings。json里面，然后拿出来
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app
+            , IHostingEnvironment env,
+            IConfiguration configUration )
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.Run(async (context) =>
+            {
+                var welcome = configUration["Welcome"];
+                await context.Response.WriteAsync(welcome);
+            });
+        }
+    }
+}
